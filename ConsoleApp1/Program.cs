c819e86d13c41972381ef514b97b2ce73b4b0767
esmaeil7 <esmaeil.hosseini727@gmail.com>
2022-05-15 15:33:28 +0430

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Parent myParent = new Parent("Message from parent.");
+            Child myChild = new Child("Message from child.");
+
+            myParent.ShowMessage();
+
+            myChild.ShowMessage();
+
+            myParent.Message = "Modified message of the parent.";
+            myParent.ShowMessage();
+
+            myChild.Message = "Modified message of the child.";
+            myChild.ShowMessage();
+
+        }
+    }
+
+
+
+    class Parent
+    {
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value; }
+        }
+
+        public void ShowMessage()
+        {
+            Console.WriteLine(message);
+        }
+
+        public Parent(string message)
+        {
+            this.message = message;
+        }
+    }
+
+    class Child : Parent
+    {
+        public Child(string message) : base(message)
+        {
+
+        }
+    }
+}
